ffb9356ae3e54ca4f5fdfe38b0812a290b99198b
Lukzard9 <lucafagiolo@gmail.com>
2026-05-27 21:57:13 +0200

    Changed kind configuration to match report

--- 3_randomness_certificate/local.tf
+++ 3_randomness_certificate/local.tf
@@ -6,8 +6,6 @@ resource "local_file" "app_env_file" {
     
     DB_PASSWORD=${random_password.db_password.result}
     
-    KIND_API_ENDPOINT=${kind_cluster.local_cluster.endpoint}
-    
     APP_HTTP_URL=http://localhost:30080
     APP_HTTPS_URL=https://localhost:30443
   EOT
--- 3_randomness_certificate/providers.tf
+++ 3_randomness_certificate/providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.30.0"
     }
-    kind = {
-      source  = "tehcyx/kind"
-      version = "~> 0.4.0" 
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.6.0"
@@ -23,14 +19,11 @@ terraform {
   }
 }
 
-provider "kind" {}
 provider "random" {}
 provider "tls" {}
 provider "local" {}
 
 provider "kubernetes" {
-  host                   = kind_cluster.local_cluster.endpoint
-  client_certificate     = kind_cluster.local_cluster.client_certificate
-  client_key             = kind_cluster.local_cluster.client_key
-  cluster_ca_certificate = kind_cluster.local_cluster.cluster_ca_certificate
+  config_path    = "~/.kube/config"
+  config_context = "kind-kind"
 }
